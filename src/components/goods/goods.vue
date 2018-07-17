ea479c6c98eed3969a272425208800bc9ad68f10
Jingfei Qin <jqin4@ucsc.edu>
2018-07-17 13:16:35 +0800

    Done with food-list css part in goods component

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -14,20 +14,20 @@
         <li v-for="item in goods" class="food-list">
           <h1 class="title">{{item.name}}</h1>
           <ul>
-            <li v-for="food in item.foods" class="food-item">
+            <li v-for="food in item.foods" class="food-item border-1px">
               <div class="icon">
-                <img :src="food.icon" alt="">
+                <img :src="food.icon" width="57px" height="57px" alt="">
               </div>
               <div class="content">
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
-                  <span>月售{{food.sellCount}}份</span>
-                  <span>好评率{{food.rating}}%</span>
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span class="rating">好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span>¥{{food.price}}</span>
-                  <span v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+                  <span class="now">¥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -40,31 +40,31 @@
 
 
 <script>
-  const ERR_OK = 0;
+const ERR_OK = 0;
 
-  export default {
-    props: {
-      seller: {
-        type: Object
-      }
-    },
-    name: "goods",
-    data() {
-      return {
-        goods: {}
-      }
-    },
-    created() {
-      this.$http.get('/api/goods').then((response) => {    //因为是mock的数据，所以一定会成功，就不写失败的call back了
-        response = response.body;                           //extract json from response
-        if (response.errno === ERR_OK) {
-          this.goods = response.data
-        }
-      });
-      this.classMap = ["decrease", "discount", "special", "invoice", "guarantee"];
+export default {
+  props: {
+    seller: {
+      type: Object
     }
-  };
-
+  },
+  name: "goods",
+  data() {
+    return {
+      goods: {}
+    };
+  },
+  created() {
+    this.$http.get("/api/goods").then(response => {
+      //因为是mock的数据，所以一定会成功，就不写失败的call back了
+      response = response.body; //extract json from response
+      if (response.errno === ERR_OK) {
+        this.goods = response.data;
+      }
+    });
+    this.classMap = ["decrease", "discount", "special", "invoice", "guarantee"];
+  }
+};
 </script>
 
 
@@ -115,6 +115,52 @@
 
     .foods-wrapper
       flex: 1
-
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        border-left: 2px solid #d9dde1
+        font-size: 12px
+        color: rgb(147, 153, 159)
+        background: #f3f5f7
+      .food-item
+        display: flex
+        margin: 18px
+        padding-bottom: 18px
+        border-1px(rgba(7, 17, 27, 0.1))
+        &:last-child
+          border-none()
+          margin-bottom: 0
+        .icon
+          flex: 0 0 57px
+          margin-right: 10px
+        .content
+          flex: 1
+          .name
+            margin: 2px 0 8px 0
+            font-size: 14px
+            line-height: 14px
+            height: 14px
+            color: rgb(7, 17, 27)
+          .desc, .extra
+            font-size: 10px
+            line-height: 10px
+            color: rgb(147, 153, 159)
+          .desc
+            margin-bottom: 8px
+          .extra
+            &.count
+              margin-right: 12px
+          .price
+            font-weight: 700
+            line-height: 24px
+            .now
+              margin-right: 8px
+              font-size: 14px
+              color: rgb(240, 20, 20)
+            .old
+              text-decoration: line-through
+              font-size: 10px
+              color: rgb(147, 153, 159)
 
 </style>
