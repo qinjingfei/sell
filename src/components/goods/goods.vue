d177d8077306f139f02b4850d34aaa2a8dc74822
Jingfei Qin <qinjingfei1@gmail.com>
2018-07-21 16:30:29 +0800

    Done with cartcontrol component

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -30,6 +30,9 @@
                   <span class="now">¥{{food.price}}</span>
                   <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
                 </div>
+                <div class="cartcontrol-wrapper">
+                  <cartcontrol :food="food"></cartcontrol>
+                </div>
               </div>
             </li>
           </ul>
@@ -44,6 +47,7 @@
 <script>
 import BScroll from "better-scroll";
 import shopcart from "@/components/shopcart/shopcart";
+import cartcontrol from "@/components/cartcontrol/cartcontrol";
 
 const ERR_OK = 0;
 
@@ -93,7 +97,8 @@ export default {
         click: true
       });
       this.foodsScroll = new BScroll(this.$refs.foodWrapper, {
-        probeType: 3
+        probeType: 3,
+        click: true
       });
       this.foodsScroll.on("scroll", pos => {
         this.scrollY = Math.abs(Math.round(pos.y));
@@ -119,112 +124,117 @@ export default {
     }
   },
   components: {
-    shopcart
+    shopcart,
+    cartcontrol
   }
 };
 </script>
 
 
 <style lang="stylus">
-@import '../../common/stylus/mixin.styl';
+@import '../../common/stylus/mixin.styl'
 
 .goods
-  display: flex;
-  overflow: hidden;
-  position: absolute;
-  top: 174px;
-  bottom: 46px;
-  width: 100%;
+  display: flex
+  overflow: hidden
+  position: absolute
+  top: 174px
+  bottom: 46px
+  width: 100%
   .menu-wrapper
-    flex: 0 0 80px;
-    width: 80px;
-    background: #f3f5f7;
+    flex: 0 0 80px
+    width: 80px
+    background: #f3f5f7
     .menu-item
-      display: table;
-      height: 54px;
-      width: 56px;
-      line-height: 14px;
-      padding: 0 12px;
+      display: table
+      height: 54px
+      width: 56px
+      line-height: 14px
+      padding: 0 12px
       &.current
-        position: relative;
-        z-index: 10;
-        margin-top: -1px;
-        background: #fff;
-        font-weight: 700;
+        position: relative
+        z-index: 10
+        margin-top: -1px
+        background: #fff
+        font-weight: 700
         .text
-          border-none();
+          border-none()
       .icon
-        display: inline-block;
-        vertical-align: top;
-        width: 12px;
-        height: 12px;
-        margin-right: 2px;
-        background-size: 12px 12px;
-        background-repeat: no-repeat;
+        display: inline-block
+        vertical-align: top
+        width: 12px
+        height: 12px
+        margin-right: 2px
+        background-size: 12px 12px
+        background-repeat: no-repeat
         &.decrease
-          bg-image('decrease_3');
+          bg-image('decrease_3')
         &.guarantee
-          bg-image('guarantee_3');
+          bg-image('guarantee_3')
         &.discount
-          bg-image('discount_3');
+          bg-image('discount_3')
         &.invoice
-          bg-image('invoice_3');
+          bg-image('invoice_3')
         &.special
-          bg-image('special_3');
+          bg-image('special_3')
       .text
-        display: table-cell;
-        width: 56px;
-        vertical-align: middle;
-        border-1px(rgba(7, 17, 27, 0.1));
-        font-size: 12px;
+        display: table-cell
+        width: 56px
+        vertical-align: middle
+        border-1px(rgba(7, 17, 27, 0.1))
+        font-size: 12px
   .foods-wrapper
-    flex: 1;
+    flex: 1
     .title
-      padding-left: 14px;
-      height: 26px;
-      line-height: 26px;
-      border-left: 2px solid #d9dde1;
-      font-size: 12px;
-      color: rgb(147, 153, 159);
-      background: #f3f5f7;
+      padding-left: 14px
+      height: 26px
+      line-height: 26px
+      border-left: 2px solid #d9dde1
+      font-size: 12px
+      color: rgb(147, 153, 159)
+      background: #f3f5f7
     .food-item
-      display: flex;
-      margin: 18px;
-      padding-bottom: 18px;
-      border-1px(rgba(7, 17, 27, 0.1));
+      display: flex
+      margin: 18px
+      padding-bottom: 18px
+      border-1px(rgba(7, 17, 27, 0.1))
       &:last-child
-        border-none();
-        margin-bottom: 0;
+        border-none()
+        margin-bottom: 0
       .icon
-        flex: 0 0 57px;
-        margin-right: 10px;
+        flex: 0 0 57px
+        margin-right: 10px
       .content
-        flex: 1;
+        flex: 1
         .name
-          margin: 2px 0 8px 0;
-          font-size: 14px;
-          line-height: 14px;
-          height: 14px;
-          color: rgb(7, 17, 27);
+          margin: 2px 0 8px 0
+          font-size: 14px
+          line-height: 14px
+          height: 14px
+          color: rgb(7, 17, 27)
         .desc, .extra
-          font-size: 10px;
-          line-height: 10px;
-          color: rgb(147, 153, 159);
+          font-size: 10px
+          line-height: 10px
+          color: rgb(147, 153, 159)
         .desc
-          margin-bottom: 8px;
-          line-height: 12px;
+          margin-bottom: 8px
+          line-height: 12px
         .extra
           .count
-            margin-right: 12px;
+            margin-right: 12px
         .price
-          font-weight: 700;
-          line-height: 24px;
+          font-weight: 700
+          line-height: 24px
           .now
-            margin-right: 8px;
-            font-size: 14px;
-            color: rgb(240, 20, 20);
+            margin-right: 8px
+            font-size: 14px
+            color: rgb(240, 20, 20)
           .old
-            text-decoration: line-through;
-            font-size: 10px;
-            color: rgb(147, 153, 159);
+            text-decoration: line-through
+            font-size: 10px
+            color: rgb(147, 153, 159)
+        .cartcontrol-wrapper
+          position: absolute
+          right: 0
+          bottom: 12px
 </style>
