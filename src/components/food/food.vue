7640cfc09da19ae2dd49e2f8b29396ed7892198b
Jingfei Qin <qinjingfei1@gmail.com>
2018-07-27 23:50:03 +0800

    Done with food.vue

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -41,6 +41,23 @@
                     <ratingselect @select="selectRating" @toggle="toggleContent" :selectType="selectType" 
                         :onlyContent="onlyContent" :desc="desc"
                         :ratings="food.ratings"></ratingselect>
+                    <div class="rating-wrapper">
+                        <ul v-show="food.ratings && food.ratings.length">
+                            <li v-show="needShow(rating.rateType,rating.text)" v-for="(rating, index) in food.ratings" class="rating-item" :key="index">
+                                <div class="user">
+                                    <span class="name">{{rating.username}}</span>
+                                    <img :src="rating.avatar" class="avatar" width="12" height="12">
+                                </div>
+                                <div class="time">{{rating.rateTime | formatDate}}</div>
+                                <p class="text">
+                                    <span :class="{'icon-thumb_up':rating.rateType === 0, 'icon-thumb_down':rating.rateType === 1}"></span>{{rating.text}}
+                                </p>
+                            </li>
+                        </ul>
+                        <div class="no-rating" v-show="!food.ratings || !food.ratings.length">
+                            暂无评价
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -49,6 +66,7 @@
 
 <script>
 import BScroll from "better-scroll";
+import {formatDate} from "@/common/js/date.js"
 import cartcontrol from "@/components/cartcontrol/cartcontrol"
 import split from "@/components/split/split"
 import ratingselect from "@/components/ratingselect/ratingselect"
@@ -106,15 +124,31 @@ export default {
         },
         selectRating(type) {
             this.selectType = type; 
-            this.$nextTick(() => {
+            this.$nextTick(() => {            //等DOM渲染完，在操作DOM
             this.scroll.refresh();
             });
         },
         toggleContent() {
             this.onlyContent = !this.onlyContent;
-            this.$nextTick(() => {
+            this.$nextTick(() => {              //等DOM渲染完，在操作DOM
                 this.scroll.refresh();
             });
+        },
+        needShow(type, text){
+            if(this.onlyContent && !text){
+                return false;
+            }
+            if(this.selectType === ALL){
+                return true;
+            }else{
+                return type === this.selectType;
+            }
+        }
+    },
+    filters:{
+        formatDate(time) {
+            let date = new Date(time);
+            return formatDate(date, 'yyyy-MM-dd hh:mm');
         }
     },
     components:{
@@ -234,6 +268,47 @@ export default {
             margin-left: 18px
             font-size: 14px
             color: rgb(7, 17, 27)
+        .rating-wrapper
+            padding: 0 18px
+            .rating-item
+                position: relative
+                padding: 16px 0
+                border-1px(rgba(7, 17, 27, 0.1))
+                .user
+                    position: absolute
+                    right: 0
+                    top: 16px
+                    line-height: 12px
+                    font-size: 0 // 清楚间隙
+                    .name
+                        display: inline-block
+                        margin-right: 6px
+                        vertical-align: top
+                        font-size: 10px
+                        color: rgb(147, 153, 159)
+                    .avatar
+                        border-radius: 50%
+                .time
+                    margin-bottom: 6px
+                    line-height: 12px
+                    font-size: 10px
+                    color: rgb(147, 153, 159)
+                .text
+                    line-height: 16px
+                    font-size: 12px
+                    color: rgb(7, 17, 27)
+                    .icon-thumb_up, .icon-thumb_down
+                        margin-right: 4px
+                        line-height: 16px
+                        font-size: 12px
+                    .icon-thumb_up
+                        color: rgb(0, 160, 220)
+                    .icon-thumb_down
+                        color: rgb(147, 153, 159)
+            .no-rating
+                padding: 16px 0
+                font-size: 12px
+                color: rgb(147, 153, 159)
 </style>
 
 
